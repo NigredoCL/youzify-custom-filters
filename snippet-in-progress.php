b5d1e625639b4f3ef4c8717c26a6854b095f4e78
NigredoCL <164560682+NigredoCL@users.noreply.github.com>
2024-03-22 17:36:04 -0300

    Update snippet-in-progress.php

--- snippet-in-progress.php
+++ snippet-in-progress.php
@@ -1,1 +1,32 @@
+/*For set global filters*/
+
+
+class BP_Custom_Activity_Filter {
+    function __construct() {
+        add_filter( 'bp_activity_get', array( $this, 'filter_activities_by_user_role' ), 10, 2 );
+    }
+
+
+    function filter_activities_by_user_role( $activities, $args ) {
+        if ( ! is_array( $activities['activities'] ) ) {
+            return $activities;
+        }
+
+        $filtered_activities = array();
+        foreach ( $activities['activities'] as $activity ) {
+            $user_id = $activity->user_id;
+            $user = get_userdata( $user_id );
+            if ( in_array( 'administrator', (array) $user->roles ) ) {
+                $filtered_activities[] = $activity;
+            }
+        }
+
+        $activities['activities'] = $filtered_activities;
+        $activities['total'] = count( $filtered_activities );
+
+        return $activities;
+    }
+}
+
+new BP_Custom_Activity_Filter();
 
