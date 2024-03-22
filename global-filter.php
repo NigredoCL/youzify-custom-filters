efd81237ff79630e3d2a8d86842f487d40a00b51
NigredoCL <164560682+NigredoCL@users.noreply.github.com>
2024-03-22 17:58:14 -0300

    Update global-filter.php

--- global-filter.php
+++ global-filter.php
@@ -1,7 +1,7 @@
 
 <?php
 
-//For set global filters
+//For set global filters. It works fine on functions.php
 
 class BP_Custom_Activity_Filter {
     function __construct() {
@@ -32,6 +32,49 @@ class BP_Custom_Activity_Filter {
 }
 
 new BP_Custom_Activity_Filter();
-
 ?>
+-----------------------------------------------------------------------------------------------------------------------
+<?php //Working in a "real" filter selectable in dropdown filter menu. This is not finish
+
+class BP_Custom_Activity_Filter {
+    function __construct() {
+        add_filter( 'bp_get_activity_show_filters_options', array( $this, 'edit_wall_filter' ) );
+        add_filter( 'bp_activity_get', array( $this, 'filter_activities_by_city' ), 10, 2 );
+    }
+
+    function edit_wall_filter( $filters ) {
+        // No this add the new filter on menu
+        $filters['city_1'] = __( 'City 1', 'textdomain' );
+        return $filters;
+    }
+
+    function filter_activities_by_city( $activities, $args ) {
+        if ( ! is_array( $activities['activities'] ) ) {
+            return $activities;
+        }
+
+        if ( isset( $args['filter']['object'] ) && $args['filter']['object'] === 'temuco' ) {
+            $filtered_activities = array();
+            foreach ( $activities['activities'] as $activity ) {
+                $user_id = $activity->user_id;
+                // This has to be searched in the wp_bp_xprofile_data table of each installation, the "field_id" must be entered. Ideally a checkbox with predefined options, 
+                // so that one can be sure that one is doing correct checks, in my example the desired field id is 16
+                $ciudad = xprofile_get_field_data( 16, $user_id );
+                
+                if ( $ciudad === 'City 1' ) {
+                    $filtered_activities[] = $activity;
+                }
+            }
+
+            $activities['activities'] = $filtered_activities;
+            $activities['total'] = count( $filtered_activities );
+        }
+
+        return $activities;
+    }
+}
+
+new BP_Custom_Activity_Filter();
+
+
 
